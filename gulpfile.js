43e4b18ee7b281ce7e4f4792af5c5fdadd258b70
lucas menezes <menezeslucas24082002@gmail.com>
2024-03-31 10:00:15 -0300

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify');
+const obfuscate = require('gulp-obfuscate');
+const imagemin = require('gulp-imagemin');
+
+function comprimeImagens() {
+    return gulp.src('./source/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./build/images'))
+}
+
+function comprimeJavascript() {
+    return gulp.src('./source/scripts/*.js')
+        .pipe(uglify())
+        .pipe(obfuscate())
+        .pipe(gulp.dest('./build/scripts'))
+}
+
+function compilaSass() {
+    return gulp.src('./source/styles/main.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass( {
+            outputStyle: 'compressed'
+        }))
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./build/styles'))
+}
+
+function funcaoPadrao(callback) {
+    console.log('Executando via Gulp');
+    funcaoPrivada();
+    callback();
+}
+
+function funcaoPrivada() {
+    console.log('Sou uma função privada');
+}
+
+exports.default = gulp.series(funcaoPadrao);
+exports.sass = compilaSass;
+exports.javascript = comprimeJavascript;
+exports.images = comprimeImagens;
---
+++ source/scripts/somar.js
@@ -0,0 +1,3 @@
+const somar = require('./math');
+
+console.log('Resultado da soma:', somar(238, 932));
