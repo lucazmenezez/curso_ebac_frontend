27ec7218ed22f1173eb93f3286dd1ec64545e3fd
lucas menezes <menezeslucas24082002@gmail.com>
2024-06-24 09:11:11 -0300

    Tratando exceções

--- main.js
+++ main.js
@@ -8,16 +8,23 @@ document.addEventListener('DOMContentLoaded', function() {
     const linkGithub = document.getElementById('link-github');
 
     fetch('https://api.github.com/users/lucazmenezez')
-    .then(function(resposta) {
-        return resposta.json();
-    })
-    .then(function(json) {
-        profileImage.src = json.avatar_url;
-        name.innerText = json.name;
-        username.innerText = json.login;
-        repositories.innerText = json.public_repos;
-        followers.innerText = json.followers;
-        following.innerText = json.following;
-        linkGithub.href = json.html_url;
-    })
-})
+        .then(function(resposta) {
+            if (!resposta.ok) {
+                throw new Error('Nome de usuário não encontrado');
+            }
+            return resposta.json();
+        })
+        .then(function(json) {
+            profileImage.src = json.avatar_url;
+            name.innerText = json.name;
+            username.innerText = json.login;
+            repositories.innerText = json.public_repos;
+            followers.innerText = json.followers;
+            following.innerText = json.following;
+            linkGithub.href = json.html_url;
+        })
+        .catch(function(error) {
+            alert('Ocorreu um erro ao buscar os dados, tente novamente mais tarde');
+            console.error('Erro:', error);
+        })
+});
