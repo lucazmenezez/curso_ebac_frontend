cee9ee790f0d2f568de259b4573eda87102e509b
lucas menezes <menezeslucas24082002@gmail.com>
2024-07-27 11:27:18 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,18 @@
+function Objeto(nomeAluno, notaAluno) {
+    this.nome = nomeAluno;
+    this.nota = notaAluno;
+}
+
+const aluno1 = new Objeto("Marcio", 6);
+const aluno2 = new Objeto("Igor", 2);
+const aluno3 = new Objeto("Edson", 9);
+const aluno4 = new Objeto("Ana", 5);
+const aluno5 = new Objeto("Carlos", 8);
+
+const arrayAlunos = [aluno1, aluno2, aluno3, aluno4, aluno5];
+
+arrayAlunos.forEach(aluno => {
+    if (aluno.nota >= 6) {
+        console.log(aluno.nome);
+    }
+})
