ddd8d236f14dd0c7216699bc8726fdd90c484f9b
lucas menezes <menezeslucas24082002@gmail.com>
2024-07-28 09:05:30 -0300

    Adicionando a função filter()

--- main.js
+++ main.js
@@ -11,8 +11,6 @@ const aluno5 = new Objeto("Carlos", 8);
 
 const arrayAlunos = [aluno1, aluno2, aluno3, aluno4, aluno5];
 
-arrayAlunos.forEach(aluno => {
-    if (aluno.nota >= 6) {
-        console.log(aluno.nome);
-    }
-})
+const alunosAprovados = arrayAlunos.filter((aluno => aluno.nota >= 6));
+
+alunosAprovados.forEach(aluno => console.log(`O aluno ${aluno.nome} foi aprovado.`));
