c89b96cc5580fe8d5d26b8b4cb20272b5e97a776
lucas menezes <menezeslucas24082002@gmail.com>
2024-08-13 07:53:12 -0300

    First Commit

---
+++ src/exercicio.js
@@ -0,0 +1,11 @@
+"use strict";
+// function multiplicacaoo(n1: number, n2: number): number {
+//     return n1 * n2;
+// }
+const multiplicacao = (n1, n2) => n1 * n2;
+function saudacao(nome) {
+    return `Olá ${nome}`;
+}
+const meuNome = 'Lucas menezes';
+console.log(multiplicacao(10, 10));
+console.log(saudacao(meuNome));
---
+++ src/exercicio.ts
@@ -0,0 +1,14 @@
+// function multiplicacaoo(n1: number, n2: number): number {
+//     return n1 * n2;
+// }
+
+const multiplicacao = (n1: number, n2: number): number => n1 * n2;
+
+function saudacao(nome: string): string {
+    return `Olá ${nome}`;
+}
+
+const meuNome = 'Lucas menezes';
+
+console.log(multiplicacao(10, 10));
+console.log(saudacao(meuNome));
